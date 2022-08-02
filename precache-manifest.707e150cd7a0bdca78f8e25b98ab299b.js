396b426a587602048de85e7188211fa93202eedc
Your Name <you@example.com>
2022-08-02 19:05:44 +0300

    Updates

---
+++ precache-manifest.707e150cd7a0bdca78f8e25b98ab299b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58efb7ebbd29de73e9ad",
+    "url": "/rick-and-morti/static/css/main.70cdf236.chunk.css"
+  },
+  {
+    "revision": "58efb7ebbd29de73e9ad",
+    "url": "/rick-and-morti/static/js/main.58efb7eb.chunk.js"
+  },
+  {
+    "revision": "1919246ebf4e0288932e",
+    "url": "/rick-and-morti/static/css/1.97910467.chunk.css"
+  },
+  {
+    "revision": "1919246ebf4e0288932e",
+    "url": "/rick-and-morti/static/js/1.1919246e.chunk.js"
+  },
+  {
+    "revision": "f29d5031fe2f57d6cd06",
+    "url": "/rick-and-morti/static/js/runtime~main.f29d5031.js"
+  },
+  {
+    "revision": "90ebdc655e88e1831cc86694435987e4",
+    "url": "/rick-and-morti/index.html"
+  }
+];
